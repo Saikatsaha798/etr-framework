a9697e3eb250e85ca3b86cce365e458272000866
Saikat Saha <saikatsaha798@gmail.com>
2026-03-29 19:45:38 +0530

    Update KafkaProducer.java

--- etr-common-kafka/src/main/java/com/java/producer/KafkaProducer.java
+++ etr-common-kafka/src/main/java/com/java/producer/KafkaProducer.java
@@ -3,7 +3,7 @@ package com.java.producer;
 /**
  * The interface Kafka producer.
  */
-public interface KafkaProducer {
+public interface KafkaProducer<T> {
 
     /**
      * Publish.
@@ -12,5 +12,5 @@ public interface KafkaProducer {
      * @param key     the key
      * @param message the message
      */
-    void publish(String topic, String key, Object message);
+    void publish(String topic, String key, T message);
 }
